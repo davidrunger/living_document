63f5f3c18f03322b84bd4bb7b6fb8309118708ad
David Runger <davidjrunger@gmail.com>
2025-01-28 22:53:38 -0600

    Remove frontmatter-related code [LD-6]
    This was something that we needed before in order to support capturing
    `puts` output. Instead, though, now we monkeypatch puts in the gem,
    rather than requiring the user to add it as frontmatter. See this diff:
    https://github.com/davidrunger/living_document/commit/faa9afbe#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5
    .
    
    The web app also used the "frontmatter" concept/code, but the web app
    was removed in https://github.com/davidrunger/living_document/pull/592
    .
    
    Therefore, we do not get value from this "frontmatter" stuff anymore,
    and so this change removes it.

--- spec/living_document/document_evaluator_spec.rb
+++ spec/living_document/document_evaluator_spec.rb
@@ -4,79 +4,75 @@ require 'spec_helper'
 
 RSpec.describe LivingDocument::DocumentEvaluator do
   subject(:document_evaluator) do
-    LivingDocument::DocumentEvaluator.new(document:, frontmatter:)
+    LivingDocument::DocumentEvaluator.new(document:)
   end
 
   describe '#evaluated_document' do
     subject(:evaluated_document) { document_evaluator.evaluated_document }
 
-    context 'when the `frontmatter` is an empty string' do
-      let(:frontmatter) { '' }
-
-      context 'when the `document` is just Ruby code' do
-        let(:document) do
-          <<~RUBY
-            2 ** 5
-            ###
-          RUBY
-        end
-
-        let(:expected_evaluated_document) do
-          <<~RUBY
-            2 ** 5
-            # => 32
-          RUBY
-        end
-
-        specify { expect(evaluated_document).to eq(expected_evaluated_document) }
-
-        it 'does not modify the original document string/object' do
-          expect(document).to include('###')
-        end
+    context 'when the `document` is just Ruby code' do
+      let(:document) do
+        <<~RUBY
+          2 ** 5
+          ###
+        RUBY
       end
 
-      context 'when the `document` is markdown containing Ruby codeblocks' do
-        let(:document) do
-          <<~MARKDOWN
-            This is how you do addition in Ruby:
+      let(:expected_evaluated_document) do
+        <<~RUBY
+          2 ** 5
+          # => 32
+        RUBY
+      end
 
-            ```rb
-            2 + 3
-            ###
-            ```
+      specify { expect(evaluated_document).to eq(expected_evaluated_document) }
 
-            This is how you do exponentiation in Ruby:
+      it 'does not modify the original document string/object' do
+        expect(document).to include('###')
+      end
+    end
 
-            ```ruby
-            2 ** 4
-            ###
-            ```
-          MARKDOWN
-        end
+    context 'when the `document` is markdown containing Ruby codeblocks' do
+      let(:document) do
+        <<~MARKDOWN
+          This is how you do addition in Ruby:
 
-        let(:expected_evaluated_document) do
-          <<~MARKDOWN
-            This is how you do addition in Ruby:
+          ```rb
+          2 + 3
+          ###
+          ```
 
-            ```rb
-            2 + 3
-            # => 5
-            ```
+          This is how you do exponentiation in Ruby:
 
-            This is how you do exponentiation in Ruby:
+          ```ruby
+          2 ** 4
+          ###
+          ```
+        MARKDOWN
+      end
 
-            ```ruby
-            2 ** 4
-            # => 16
-            ```
-          MARKDOWN
-        end
+      let(:expected_evaluated_document) do
+        <<~MARKDOWN
+          This is how you do addition in Ruby:
 
-        specify { expect(evaluated_document).to eq(expected_evaluated_document) }
+          ```rb
+          2 + 3
+          # => 5
+          ```
 
-        it 'does not modify the original document string/object' do
-          expect(document).to include('###')
-        end
+          This is how you do exponentiation in Ruby:
+
+          ```ruby
+          2 ** 4
+          # => 16
+          ```
+        MARKDOWN
+      end
+
+      specify { expect(evaluated_document).to eq(expected_evaluated_document) }
+
+      it 'does not modify the original document string/object' do
+        expect(document).to include('###')
       end
     end
   end
